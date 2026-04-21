9eff8a647513228ba4255994006e7496087578e7
Migue <migueesquivelrios@gmail.com>
2026-04-21 16:43:33 -0300

    agregamos scss

--- icard_react/icard/src/App.js
+++ icard_react/icard/src/App.js
@@ -1,27 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-//import { Button } from 'semantic-ui-react';
-
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Button>Click Here</Button>
-      </header>
-
+    <div className="app">
+        <h1 className='app'>Welcome to iCard!</h1>
     </div>
   );
 }
