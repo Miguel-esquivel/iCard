f0908cd9275fc603f0001090e9ed160033afb7fc
Migue <migueesquivelrios@gmail.com>
2026-04-23 17:56:08 -0300

    27 Enpoint para obtener los datos personales

--- icard_django/icard/users/api/router.py
+++ icard_django/icard/users/api/router.py
@@ -1,11 +1,14 @@
 
-#from django.urls import path
+from django.urls import path
 from rest_framework.routers import DefaultRouter
-from users.api.views import UserApiViewSet
-#from users.api.router import router_users
+from users.api.views import UserApiViewSet, UserView
 
 router_user = DefaultRouter()
 
 router_user.register(
     prefix='users', basename='users', viewset=UserApiViewSet
     )
+
+urlpatterns = [
+    path('auth/me/', UserView.as_view()),
+]
--- icard_django/icard/users/api/views.py
+++ icard_django/icard/users/api/views.py
@@ -1,9 +1,12 @@
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
+from rest_framework.response import Response
 from django.contrib.auth.hashers import make_password
 
 from users.models import User
 from users.api.serializers import UserSerializer
+
 
 class UserApiViewSet(ModelViewSet):
     permission_classes = [IsAdminUser]  
@@ -28,3 +31,13 @@ class UserApiViewSet(ModelViewSet):
             request.data['password'] = request.user.password # Eliminar el campo password si no se proporciona una nueva contraseña
         return super().update(request, *args, **kwargs)
     
+
+
+
+# 👇 ESTA CLASE VA AFUERA
+class UserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = UserSerializer(request.user)
+        return Response(serializer.data)
